1a31a3c97d171eab603054299f39417f2f07ce68
quizchi <qwertyozb18@gmail.com>
2026-02-04 20:45:18 +0500

    fox import error

--- database/__init__.py
+++ database/__init__.py
@@ -3,7 +3,7 @@ from .db import (
     get_session_results, close_session, get_questions, add_question,
     get_questions_count, get_group_rating, get_global_rating,
     get_top_users, reset_all_coins, get_user_rank, search_questions, delete_question,
-    questions_table_exists, get_user_stats, get_ranking_by_period, get_exchange_rate,
+    get_user_stats, get_ranking_by_period, get_exchange_rate,
     set_exchange_rate, create_withdrawal, get_pending_withdrawals, update_withdrawal_status,
     get_admin_dashboard_stats, get_custom_subjects_list, add_custom_subject, remove_custom_subject
 )
